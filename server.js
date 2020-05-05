7b2de56796510535b45e7c35ab7189aa820823ee
Neryad <dayern.gomez@hotmail.com>
2020-05-05 13:14:37 -0400

    adding morga to log on dev enviroment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 // Routes files
 
@@ -9,6 +11,12 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// Dev loggin moddlware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+// app.use(logger);
 
 // Mount Routeers
 app.use('/api/v1/bootcamps', bootcamps);
