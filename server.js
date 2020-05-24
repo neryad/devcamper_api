c4fd0eadb7669018ae70e429dfce161622e8fae5
Neryad <dayern.gomez@hotmail.com>
2020-05-24 00:08:34 -0400

    limert and h

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 const errorHandler = require('./middleware/error');
 
 const connectDB = require('./config/db');
@@ -47,8 +49,18 @@ app.use(mongoSanitize());
 app.use(helmet());
 
 // prevent xss attacs
-
 app.use(xss());
+
+// Rate limit
+const limiter = rateLimit({
+    windowMs: 10 * 60 *1000,
+    max:100
+});
+
+app.use(limiter);
+
+//Prevent http param pollution
+app.use(hpp());
 //set static folder
 app.use(express.static(path.join(__dirname,'public')));
 // app.use(logger);
